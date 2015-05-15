0e3d6f96c77b57fbb87b5a38076c8a465e6abb3e
Brandon Taylor <brandon@iambrandontaylor.com>
2015-05-14 23:38:02 -0400

    Added unit test for transposition of values if min > max.

--- test/generator.js
+++ test/generator.js
@@ -18,4 +18,20 @@ describe('Random Number Generator', function() {
             }
         });
     });
+
+    describe('When generating random numbers and the min/mx are reversed', function() {
+        it('should transpose the values and return an array of two integers', function() {
+            var options = {
+                    min: 100,
+                    max: 1
+                },
+                randomInts = getRandomInts(options);
+            assert.equal(randomInts.length, 2);
+
+            // assert integers are below the maximum default of 100
+            for (var i=0; i<randomInts.length; i++) {
+                assert(randomInts[i] < options.max);
+            }
+        });
+    });
 });
