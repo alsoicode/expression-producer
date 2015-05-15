b0232b2c754451dcdba86746ab64ccbf9d1e459a
Brandon Taylor <brandon@iambrandontaylor.com>
2015-05-15 00:04:53 -0400

    Added logging.

--- routes/producer.js
+++ routes/producer.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var getRandomInts = require('../lib/generator');
+var getRandomInts = require('../lib/generator'),
+    logger = require('../logger');
 
 exports.generateExpression = function(req, res, next) {
     
     var randomIntegers = getRandomInts(),
         expression = randomIntegers.join('+') + '=';
+
+    logger.log('info', 'Expression %s generated.', expression);
 
     res.json(201, {'expression': expression});
 
